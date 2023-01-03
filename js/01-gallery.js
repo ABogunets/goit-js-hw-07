58414ecb3b871d9c8bd8e219a44af5af879e0dcb
ABogunets <abogunets@gmail.com>
2023-01-03 23:56:49 +0200

    task1 started
    MarkUp done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+const galleryRef = document.querySelector('.gallery');
+
+//Gallery Items Markup
+const galleryItemsMarkup = createGalleryItemsMarkUp(galleryItems);
+// console.log("galleryItemsMarkup", galleryItemsMarkup);
+
+galleryRef.innerHTML = galleryItemsMarkup;
+
+function createGalleryItemsMarkUp(items) {
+  return items.map(({ preview, original, description}) => `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`).join('');
+}
+
+// Click listener
+galleryRef.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+  console.log(e.target);
+}
